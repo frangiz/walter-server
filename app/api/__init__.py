5b5c0974a5ff7a0912eb56527f31042461637f19
Björn <frangiz@gmail.com>
2020-05-01 12:46:58 +0200

    Added support for OTA updates.

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -2,4 +2,4 @@ from flask import Blueprint
 
 bp = Blueprint("api", __name__)
 
-from app.api import sensors, errors
+from app.api import sensors, errors, firmware
---
+++ app/api/firmware.py
@@ -0,0 +1,53 @@
+import os
+import glob
+from flask import jsonify, request, current_app, send_from_directory
+
+from app.api import bp
+from app.api.errors import error_response, bad_request
+
+
+@bp.route("/firmware/updates", methods=["GET"])
+def get_new_firmware():
+    ver = request.args.get("ver", default=None)
+    if ver is None or not ver.isdigit():
+        return bad_request("Required parameter 'ver' is missing or not an int.")
+
+    dev_type = request.args.get("dev_type", default=None)
+    if dev_type is None:
+        return bad_request("Required parameter 'dev_type' is missing.")
+    dev_type = dev_type.lower()
+
+    dev_id = request.args.get("dev_id", default=None)
+    if dev_id is None:
+        return bad_request("Required parameter 'dev_id' is missing.")
+    dev_id = dev_id.lower()
+
+    current_app.logger.debug(
+        "ver: " + ver + ", dev: " + dev_type + " dev_id: " + dev_id
+    )
+    # TODO: Update sensor if found with current firmware version
+
+    latest_firmware = find_latest_firmware(ver, dev_type)
+    if latest_firmware is None:
+        current_app.logger.debug("Device already up to date")
+        return error_response(304, "Device already up to date")
+
+    current_app.logger.debug("Found firmware version: " + latest_firmware)
+    return send_from_directory(
+        directory="../firmwares",
+        filename=latest_firmware,
+        as_attachment=True,
+        mimetype="application/octet-stream",
+        attachment_filename=latest_firmware,
+    )
+
+
+def find_latest_firmware(ver, dev_type):
+    if not os.path.exists("firmwares"):
+        os.makedirs("firmwares")
+    firmwares = sorted(glob.glob("firmwares/" + dev_type + "-*.bin"), reverse=True)
+    if len(firmwares) > 0:
+        firmware_filename = firmwares[0].replace("firmwares\\", "")
+        if firmware_filename != dev_type + "-" + str(ver) + ".bin":
+            return firmware_filename
+    return None
